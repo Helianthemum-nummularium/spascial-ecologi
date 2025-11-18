c00a94d37d3b812fe7d648dccdea66616aa2405e
Helianthemum-nummularium <helianthemum.obscurum@gmail.com>
2025-11-18 16:32:25 +0100

    Update 05_community_overlap.R

--- code/05_community_overlap.R
+++ code/05_community_overlap.R
@@ -8,7 +8,15 @@ data(kerinci)
 
 sunazimuth <- kerinci$Time * 2 * pi
 kerinci$azimuth <- kerinci$Time * 2 * pi
-tiger <- kerinci[kerinci$Sps=="tiger",]
+tiger <- kerinci[kerinci$Sps=="tiger",-3]
 tigertime <- tiger$azimuth
 
 densityPlot(tigertime)
+
+# sac ma caque
+
+macaque <- kerinci[kerinci$Sps=="macaque",-3]
+macaquetime <- macaque$azimuth
+
+densityPlot(macaquetime)
+overlapPlot( macaquetime, tigertime)
